c83a44e7d90a2c6d5a9e207e89d11097c07e0966
Pedro Silva <pedrildojw@gmail.com>
2023-01-31 13:41:54 -0300

    Adicionar script do Carousel em arquivo externo

--- script/myscript.js
+++ script/myscript.js
@@ -1,1 +1,19 @@
-
+$('.slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000, //2000ms = 2s;
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+});
